03c61fc791ca9133630d914c69d4556c997471e0
MR AĞA <99508889+tecnoagateam@users.noreply.github.com>
2023-03-24 11:44:33 +0400

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL" none)
 OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
